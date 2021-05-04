88183a08192b5a55b47b1860d5b73f48021cfd77
Bogdan Baciu <bogdanbaciu00@yahoo.com>
2021-05-04 13:26:26 +0300

    Added customer model

---
+++ sef_proj/app/src/main/java/com/example/wusbee/CustomerModel.java
@@ -0,0 +1,55 @@
+package com.example.wusbee;
+
+public class CustomerModel {
+    private String FullName;
+    private String Username;
+    private String Password;
+    private String Email;
+    private int PhoneNumber;
+
+    public CustomerModel(String fullName, String username, String password, String email, int phoneNumber)  {
+        FullName = fullName;
+        Username = username;
+        Password = password;
+        Email = email;
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getFullName() {
+        return FullName;
+    }
+
+    public void setFullName(String fullName) {
+        FullName = fullName;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public int getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) { PhoneNumber = phoneNumber; }
+}
--- sef_proj/app/src/main/java/com/example/wusbee/SignInActivity.java
+++ sef_proj/app/src/main/java/com/example/wusbee/SignInActivity.java
@@ -3,12 +3,51 @@ package com.example.wusbee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignInActivity extends AppCompatActivity {
+
+    Button CreateAccButton;
+    EditText FullName;
+    EditText UserName;
+    EditText Email;
+    EditText PassWord;
+    EditText PhoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        CreateAccButton = findViewById(R.id.create_acc_button);
+        FullName = findViewById(R.id.full_name);
+        UserName = findViewById(R.id.user_name);
+        Email = findViewById(R.id.email);
+        PassWord = findViewById(R.id.password);
+        PhoneNumber = findViewById(R.id.phone_number);
+
+        CreateAccButton.setOnClickListener(v -> { //add to db
+
+            try {
+                CustomerModel customerModel = new CustomerModel(
+                        FullName.getText().toString(),
+                        UserName.getText().toString(),
+                        Email.getText().toString(),
+                        PassWord.getText().toString(),
+                        Integer.parseInt(PhoneNumber.getText().toString())
+                );
+            }catch(Exception exception){
+                    Toast.makeText(this, "Registration Failed!", Toast.LENGTH_SHORT).show();
+                }
+
+            Toast.makeText(this, "Successful Registration!", Toast.LENGTH_SHORT).show();
+
+        });
+
     }
 }
